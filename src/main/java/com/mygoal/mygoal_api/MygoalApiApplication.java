624787b7435a8a67f7cfdc358e189470d39dbca8
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2023-07-21 13:25:55 -0500

    initial commit

---
+++ src/main/java/com/mygoal/mygoal_api/MygoalApiApplication.java
@@ -0,0 +1,13 @@
+package com.mygoal.mygoal_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MygoalApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MygoalApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mygoal/mygoal_api/MygoalApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mygoal.mygoal_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MygoalApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
