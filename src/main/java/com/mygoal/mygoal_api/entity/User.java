e7ffdd29c928c274cd1d7e5a9303336d2a975c7e
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2023-07-24 23:42:42 -0500

    add folder entity and user as new entity

---
+++ src/main/java/com/mygoal/mygoal_api/entity/User.java
@@ -0,0 +1,37 @@
+package com.mygoal.mygoal_api.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+
+@Entity
+public class User {
+    @Column
+    private String email;
+    @Column
+    private String password;
+
+    public User(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public User() {
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
