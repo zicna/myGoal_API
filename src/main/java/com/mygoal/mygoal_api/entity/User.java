42043ec859ec871a47768e693f85cad84d2cc6f1
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2023-07-25 10:20:20 -0500

    add id field, clean up entity

--- src/main/java/com/mygoal/mygoal_api/entity/User.java
+++ src/main/java/com/mygoal/mygoal_api/entity/User.java
@@ -2,9 +2,17 @@ package com.mygoal.mygoal_api.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     @Column
     private String email;
     @Column
@@ -16,6 +24,14 @@ public class User {
     }
 
     public User() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getEmail() {
