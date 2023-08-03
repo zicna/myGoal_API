c96c2553763d181209877d1f7ede2fabe92bba1c
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2023-08-03 11:54:55 -0500

    add new method to handle wrong user exception thrown

--- src/main/java/com/mygoal/mygoal_api/ApplicationExceptionHandler.java
+++ src/main/java/com/mygoal/mygoal_api/ApplicationExceptionHandler.java
@@ -6,13 +6,20 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import com.mygoal.mygoal_api.exception.UserNotFoundException;
+import com.mygoal.mygoal_api.exception.WrongUserInputException;
 
 @ControllerAdvice
 public class ApplicationExceptionHandler {
 
     @ExceptionHandler(UserNotFoundException.class)
-    public ResponseEntity<String> userNotFoundByEmailExcHandler(UserNotFoundException ex){
+    public ResponseEntity<String> userNotFoundByEmailExcHandler(UserNotFoundException ex) {
         return new ResponseEntity<String>(ex.getMessage(), HttpStatus.BAD_REQUEST);
     }
-    
+
+    @ExceptionHandler(WrongUserInputException.class)
+    public ResponseEntity<String> wrongUserInputExcHandler(WrongUserInputException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    }
 }
+
+
