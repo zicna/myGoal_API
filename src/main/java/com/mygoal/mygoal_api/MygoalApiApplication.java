7693c0c3db64eca8378f82aaac887518bbf45145
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2023-08-17 11:43:05 -0500

    update TODO's

--- src/main/java/com/mygoal/mygoal_api/MygoalApiApplication.java
+++ src/main/java/com/mygoal/mygoal_api/MygoalApiApplication.java
@@ -4,9 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 // !Goal enpoints
-// TODO: 1. create method to retrive single goal
-// TODO: 1.1 create exception for: goal not found
-// TODO: 1.2 create exception for: goal found but user_id does not match with goals user id
 // TODO: 1.3 handle newly created exceptions
 // TODO: 1.4 refactor methods in GoalServiceImpl 
 
