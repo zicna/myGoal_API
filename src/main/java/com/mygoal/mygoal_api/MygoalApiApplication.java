44c443d3f22faedf1cd6f445eeb8c66c6cd8084f
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2023-08-24 12:22:10 -0500

    remove GoalParent, GoalRequest is not subclass of GoalParent

--- src/main/java/com/mygoal/mygoal_api/MygoalApiApplication.java
+++ src/main/java/com/mygoal/mygoal_api/MygoalApiApplication.java
@@ -4,8 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 // ! Refactor 
-// TODO: Goal Controller
-// TODO: Goal ServiceImpl
 
 // ! Validation
 // TODO: Add validation for GoalRequest
